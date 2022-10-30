2a43d886a3248b90c0b9b37682a924de429f09f6
Jae Yeong Park <sunskyking@naver.com>
2022-10-29 21:58:25 -0700

    second commit

--- nodes/my_python_node.py
+++ nodes/my_python_node.py
@@ -3,8 +3,8 @@
 import sys
 import rospy
 import actionlib
-from geometry_msgs.msg import Twist, P
-
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import quaternion_from_euler
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
@@ -16,15 +16,15 @@ print(client.wait_for_server())
 
 def update_init_pose(self, x, y, theta):
     init_pose = PoseWithCovarianceStamped()
-    self.init_pose.header.stamp = rospy.Time.now()
-    self.init_pose.pose.pose.position.x = x
-    self.init_pose.pose.pose.position.y = y
-    self.init_pose.pose.pose.orientation.w = 1.0
+    init_pose.header.stamp = rospy.Time.now()
+    init_pose.pose.pose.position.x = x
+    init_pose.pose.pose.position.y = y
+    init_pose.pose.pose.orientation.w = 1.0
     q = quaternion_from_euler(0.0, 0.0, theta)
-    self.init_pose.pose.pose.orientation.x = q[0]
-    self.init_pose.pose.pose.orientation.y = q[1]
-    self.init_pose.pose.pose.orientation.z = q[2]
-    self.init_pose.pose.pose.orientation.w = q[3]
+    init_pose.pose.pose.orientation.x = q[0]
+    init_pose.pose.pose.orientation.y = q[1]
+    init_pose.pose.pose.orientation.z = q[2]
+    init_pose.pose.pose.orientation.w = q[3]
     pub_init.publish(init_pose)
 
 def send_goal(x, y, theta):
@@ -35,19 +35,20 @@ def send_goal(x, y, theta):
     goal.target_pose.pose.position.y = y
     quat = quaternion_from_euler(0.0, 0.0, theta)
     goal.target_pose.pose.orientation.x = quat[0]
-    goal.target_pose.pose.orientation.y = quat[0]
-    goal.target_pose.pose.orientation.z = quat[0]
-    goal.target_pose.pose.orientation.w = quat[0]
+    goal.target_pose.pose.orientation.y = quat[1]
+    goal.target_pose.pose.orientation.z = quat[2]
+    goal.target_pose.pose.orientation.w = quat[3]
     wait = client.wait_for_result()
     if not wait:
         print('Error')
     else:
         print(client.get_result())
 
-update_init_pose(-2.0, -0.3, 0.0)
-send_goal(3.0, 3.0, 0.0)
+update_init_pose(0.0, 0.0, 0.0, 0.0)
+send_goal(-0.5, -0.3, 0.0)
 #Added another waypoint
-send_goal(-1.0, 1.0, 0.0)
+send_goal(-0.5, -0.3, 0.0)
+
 send_goal(-2.0, -0.3, 0.0)
 
 def update_timer(timer_event):
